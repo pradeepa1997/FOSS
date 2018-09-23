08cba6e645578c171f70d1a9f610c33b7e5ee9e7
pradeepa1997 <pradeepasandharuwan@gmail.com>
2018-09-23 14:16:07 +0530

    first commit

---
+++ src/foss/code/FOSSCODE.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package foss.code;
+
+/**
+ *
+ * @author P P Sandaruwan
+ */
+public class FOSSCODE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   // TODO code application logic here
+   System.out.println("Hello world");
+   System.out.println(5+5);
+   int i;
+   for(i=0;i<10;i++){
+       System.out.println(i);
+       
+   }
+   while(i>0){
+       System.out.println(i);
+       i--;
+   }
+    }
+    
+}
